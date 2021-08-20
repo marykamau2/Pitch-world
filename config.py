e1dbf746da3f587ff12743880ad9761dead19bf0
marykamau2 <mary.kamau@student.moringaschool.com>
2021-08-20 22:58:52 +0300

    recreated a database

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
-# import os
+import os
 
-# class Config:
+class Config:
    
-#     SECRET_KEY = "MARY102"
-#     # # UPLOAD_PHOTOS_DEST = 'app/static/photos'
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SECRET_KEY = "MARY102"
+    # # UPLOAD_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
-# class ProdConfig(Config):
-#     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://marynjeri:njeri2018@localhost/pitches'
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://marynjeri:njeri2018@localhost/pitches'
 
-# class DevConfig(Config):
+class DevConfig(Config):
     
-#     DEBUG = True
+    DEBUG = True
 
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig
-# }
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}

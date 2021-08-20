7aecf4667504cab3c90aed822e7033cad02bf8df
marykamau2 <mary.kamau@student.moringaschool.com>
2021-08-20 22:37:46 +0300

    make imports required for login setup

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,8 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField,PasswordField,SubmitField
-# from wtforms.validators import Required,Email,EqualTo, ValidationError
-# from ..models import User
-# from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Required,Email,EqualTo, ValidationError
+from ..models import User
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 
 # class RegistrationForm(FlaskForm):
 #     email = StringField('Your Email Address',validators=[Required(),Email()])
